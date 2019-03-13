c83573b50539886cff496aba09c1602ad176422a
Michael Greer <michael.greer@bmwgroup.com>
2019-03-13 16:23:56 -0400

    fixed validate_dns script bug

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,9 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [0.2.1] 2019-03-12
+## [0.2.1] 2019-03-13
 ### Added
 - tagging public subnets via `public_subnets` variable
+- fixed bug in validate_dns script reporting incorrect state
 
 ## [0.2.0] [2019-03-04]
 ### Added
--- scripts/validate_dns.sh
+++ scripts/validate_dns.sh
@@ -16,24 +16,43 @@ if [ "$#" -eq 3 ]; then
 else
     MAX_ATTEMPTS=40
 fi
+if [ "$#" -eq 4 ]; then
+    DEBUG=$4
+else
+    DEBUG=0
+fi
 
 echo "DNS readiness probe is using   : $KUBECONFIG"
 echo "DNS readiness probe interval is: $POOL_WAIT_SECONDS seconds"
 echo "DNS maximum readiness attempts : $MAX_ATTEMPTS times"
 echo "Maximum wait time for DNS pods to come on-line: $(expr ${POOL_WAIT_SECONDS} \* ${MAX_ATTEMPTS}) seconds."
 sleep ${POOL_WAIT_SECONDS}
-nodes=$(kubectl --kubeconfig=$KUBECONFIG get pod --all-namespaces 2>&1)
+nodes=$(kubectl --kubeconfig=$KUBECONFIG get pod -n kube-system 2>&1)
+if [[ "${DEBUG}" ]]; then
+    echo "DEBUG:\n${nodes}\n"
+fi
 #nodes=$(cat nodes.txt)  # for internal testing 
 running_dns=$(echo -n "${nodes}" | grep 'kube-dns' | grep ' Running '| wc -l)
+if [[ "${DEBUG}" -gt 0 ]]; then
+    echo "DEBUG:\n${running_dns}\n"
+    echo "${nodes}\n"
+fi
 iterator=0
 
 while [[ $running_dns -ne 1 ]]; do
     iterator=$((iterator+1))
     echo "DNS readiness probe attempt #${iterator} out of ${MAX_ATTEMPTS}"
     sleep ${POOL_WAIT_SECONDS}
-    nodes=$(kubectl --kubeconfig=$KUBECONFIG get pod --all-namespaces 2>&1)
+    nodes=$(kubectl --kubeconfig=$KUBECONFIG get pod -n kube-system 2>&1)
+    if [[ "${DEBUG}" -gt 0 ]]; then
+        echo "DEBUG:\n${nodes}\n"
+    fi
     #nodes=$(cat nodes.txt)  # for internal testing 
-    running_dns=$(echo -n "${nodes}" | grep 'kube-dns' | grep -v ' Running ' | wc -l)
+    running_dns=$(echo -n "${nodes}" | grep 'kube-dns' | grep ' Running ' | wc -l)
+    if [[ "${DEBUG}" -gt 0 ]]; then
+        echo "DEBUG:${running_dns}\n"
+        echo "${nodes}\n"
+    fi
     if [[ "${iterator}" -eq "${MAX_ATTEMPTS}" ]]; then
         echo "FAILED KUBE-DNS readiness probe ...."
         exit 1
