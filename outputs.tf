206fe354c1bd373f27b71639589b4b3ebf40f510
Michael Greer <michael.greer@bmwgroup.com>
2019-04-10 16:12:21 -0400

    added new outputs

--- outputs.tf
+++ outputs.tf
@@ -22,3 +22,53 @@ output "cluster_certificate_authority_data" {
   description = "Nested attribute containing certificate-authority-data for your cluster. This is the base64 encoded certificate data required to communicate with your cluster."
   value       = "${module.eks.cluster_certificate_authority_data}"
 }
+
+output "cluster_version" {
+  description = "The Kubernetes server version for the EKS cluster."
+  value       = "${module.eks.version}"
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster."
+  value       = "${module.eks.cluster_security_group_id}"
+}
+
+output "cluster_iam_role_name" {
+  description = "IAM role name of the EKS cluster."
+  value       = "${module.eks.cluster_iam_role_name}"
+}
+
+output "cluster_iam_role_arn" {
+  description = "IAM role ARN of the EKS cluster."
+  value       = "${module.eks.cluster_iam_role_arn}"
+}
+
+output "kubeconfig_filename" {
+  description = "The filename of the generated kubectl config."
+  value       = "${module.eks.kubeconfig_filename}"
+}
+
+output "workers_asg_arns" {
+  description = "IDs of the autoscaling groups containing workers."
+  value       = "${module.eks.workers_asg_arns}"
+}
+
+output "workers_asg_names" {
+  description = "Names of the autoscaling groups containing workers."
+  value       = "${module.eks.workers_asg_names}"
+}
+
+output "worker_security_group_id" {
+  description = "Security group ID attached to the EKS workers."
+  value       = "${module.eks.worker_security_group_id}"
+}
+
+output "worker_iam_role_name" {
+  description = "default IAM role name for EKS worker groups"
+  value       = "${module.eks.worker_iam_role_name}"
+}
+
+output "worker_iam_role_arn" {
+  description = "default IAM role ARN for EKS worker groups"
+  value       = "${module.eks.worker_iam_role_arn}"
+}
