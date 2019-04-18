b7184007c4c4a3ba96fa6b7ab09cf0572fb62803
Radosław Palczyński <radoslaw.palczynski@nordcloud.com>
2019-04-18 13:47:02 +0200

    vpc endpoint work

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ module "eks" {
   worker_group_count                         = "1"
   worker_additional_security_group_ids       = ["${aws_security_group.all_worker_mgmt.id}"]
   cluster_version                            = "${var.cluster_version}"
+  cluster_endpoint_private_access            = "true"
 }
 
 resource "aws_security_group" "all_worker_mgmt" {
